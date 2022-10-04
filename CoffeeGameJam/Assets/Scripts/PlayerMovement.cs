a3320359a07f083daa444bb5f449fb21c79d5ea9
macronnie <macr0nnieasmr@gmail.com>
2022-10-04 00:18:52 -0400

    make the character move and added npc detection
    square turns red when f button is pressed while near it. need to code the attack functionality

--- CoffeeGameJam/Assets/Scripts/PlayerMovement.cs
+++ CoffeeGameJam/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+    private float horizontal;
+    [SerializeField]private float speed = 8f;
+    [SerializeField]private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    // Update is called once per frame
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    //Player stealing candy mechanic 
+    void OnTriggerStay2D(Collider2D col){
+             
+        if(Input.GetKeyDown(KeyCode.F)){
+            Debug.Log(col + "jit");
+            col.GetComponent<NPC>().isMad = true;  
+        }
+    }
+
+
 }
+
